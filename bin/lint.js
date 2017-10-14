b803667b40937d95f3d3f05e4d7adea34ba7497a
geritol <gergo.tolnai@gmail.com>
2017-10-14 18:35:45 +0200

    configured linting

---
+++ bin/lint.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const eslint = require('eslint')
+
+const CLIEngine = eslint.CLIEngine
+const linter = new CLIEngine({
+    useEslintrc: true,
+})
+const report = linter.executeOnFiles([
+    './src/*.js',
+    '!./src/*.test.js',
+    '!./src/registerServiceWorker.js',
+])
+
+const formatter = linter.getFormatter()
+const errorCheck = (errorCount, file) => errorCount + file.errorCount;
+
+console.log(formatter(report.results));
+
+if (report.results.reduce(errorCheck, 0) > 0) {
+    process.exit(1);
+}
