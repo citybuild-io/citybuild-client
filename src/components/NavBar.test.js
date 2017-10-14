427e967ceff7b6b5aecdc1a824a5e464037fb586
geritol <gergo.tolnai@gmail.com>
2017-10-14 20:26:54 +0200

    added nabber

---
+++ src/components/NavBar.test.js
@@ -0,0 +1,10 @@
+/* eslint-env node, mocha */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NavBar from './NavBar';
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<NavBar />, div);
+});
